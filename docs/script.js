14069a011ebb141dd9d6d8b5c6d576e27997f942
Tuan Phan <tuan@tuan.design>
2020-08-18 19:26:52 -0700

    Added the placeholders for the content

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+
+// 1. Building the search function for the current city
+
+function searchCity(cityname){
     
+    const queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=166a433c57516f51dfab1f7edaed8413";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        console.log(queryURL);
+    });
+
+}
+
 })
